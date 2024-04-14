4af353c556d109e5f7bb9bd99cca5ea65b5f4df6
Inderpal Singh <inderpalsmutta@gmail.com>
2024-04-14 16:39:21 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:cubit_1/screeens/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Cubit App',
+      home: HomePage(),
+    );
+  }
+}
