67df885b5faa7002acfa0043deb2d310fc5f062b
Inderpal Singh <inderpalsmutta@gmail.com>
2024-04-16 06:40:58 +0530

    Latest

--- lib/cubit/todo_cubit.dart
+++ lib/cubit/todo_cubit.dart
@@ -1,8 +1,16 @@
+import 'package:cubit_1/cubit/todo_state.dart';
+import 'package:cubit_1/models/todo_model_cubit.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class TodoCubit extends Cubit<int>{
-  TodoCubit(): super(0);
-
+class TodoCubit extends Cubit<TodoCubitState> {
+  TodoCubit() : super(TodoCubitState(todoModel: []));
   
+  
+  //// add 
+
+  List<TodoCubitModel> addTodoCubit(){
+    
+    
+  }
   
 }
---
+++ lib/cubit/todo_state.dart
@@ -0,0 +1,11 @@
+import '../models/todo_model_cubit.dart';
+
+class TodoCubitState {
+  
+  
+  List<TodoCubitModel> todoModel = [];
+
+  TodoCubitState({required this.todoModel});
+  
+  
+}
